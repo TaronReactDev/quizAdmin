3db135c7e0d84893f839a14a2cdc9311a853ea5e
TaronReactDev <taronsargsyan.reactdev@gmail.com>
2021-10-20 19:51:27 +0400

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Admin from "./admin/index"
 import Login from "./login/loginPage"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-function App() {
-  return (
-    <Login/>
-  );
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Login}/>
+                    <Route path="/admin" component={Admin}/>
+            </Switch>
+
+        </Router>
+
+);
 }
 
 export default App;
--- src/login/loginPage.js
+++ src/login/loginPage.js
@@ -1,13 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios from "axios";
 import "./loginStyles.scss"
+import {Link, Redirect} from "react-router-dom";
 
 const LoginPage = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [radio, setRadio] = useState("")
+    const [type, setType] = useState("")
 
-    const handleChangForLogin = (type, e) => () => {
+    const radioRefAdmin = useRef(null)
+    const radioRefUser = useRef(null)
+
+    const [direction, setDirection] = useState("/")
+
+
+    const handleChangForLogin = (type) => (e) => {
         switch (type) {
             case "email":
                 setEmail(e.target.value);
@@ -15,44 +22,83 @@ const LoginPage = (props) => {
             case "pass":
                 setPassword(e.target.value);
                 break;
-            case "radio":
-                setRadio(e.target.value);
+            case "radioAdmin":
+                setType(radioRefAdmin.current.value);
+                break;
+            case "radioUser":
+                setType(radioRefUser.current.value);
                 break;
         }
     }
 
-    console.log(radio)
 
-    const handleLoginSubmit = (e) => {
+    const handleLoginSubmit = async (e) => {
+
         e.preventDefault();
         const loginInfo = {
             email,
-            password
+            password,
+            type
+
+        }
+        try {
+            const res = await axios.post(`api/login/admin`, loginInfo)
+
+
+            // switch (res.data.message) {
+            //     case "isAdmin":
+            //         setPassword("");
+            //         setEmail("");
+            //         setType("");
+            //         setDirection("admin")
+            //         break;
+            //
+            //     case "isUser" :
+            //         setPassword("");
+            //         setEmail("");
+            //         setType("");
+            //         setDirection("user")
+            //         break;
+            // }
+
+            console.log( res, "res.data")
+        } catch
+            (e) {
+            console.log("request failed", e)
         }
 
-        axios.get(`api/login/admin`)
 
     }
-    return (
-        <>
-            <div className="loginContainer">
-                <form onSubmit={handleLoginSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" id="email" value={email} onChange={handleChangForLogin("email")}/>
-                    <label htmlFor="pass">Password</label>
-                    <input type="password" id="pass" value={password} onChange={handleChangForLogin("pass")}/>
-                    <div onChange={handleChangForLogin("radio")}>
 
-                        <input name="login" type="radio" value="Login as admin" />
-                        <label htmlFor="Login as admin"> Login as admin </label>
 
-                        <input name="login" type="radio" value="Login as user"/>
-                        <label htmlFor="Login as admin"> Login as user </label>
-                    </div>
-                </form>
-            < /div>
-            <button type="submit">Login</button>
-        </>
-    );
+    if (direction === "/") {
+        return (
+            <div className="pseudoBody">
+                <div className="loginContainer">
+                    <form onSubmit={handleLoginSubmit}>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" id="email" value={email} onChange={handleChangForLogin("email")}/>
+                        <label htmlFor="pass">Password</label>
+                        <input type="password" id="pass" value={password} onChange={handleChangForLogin("pass")}/>
+                        <div onChange={handleChangForLogin("radio")}>
+
+                            <input name="login" type="radio" ref={radioRefAdmin} onChange={handleChangForLogin("radioAdmin")}
+                                   value="admin"/>
+                            <label htmlFor="Login as admin"> Login as admin </label>
+
+                            <input name="login" type="radio" ref={radioRefUser} onChange={handleChangForLogin("radioUser")}
+                                   value="user"/>
+                            <label htmlFor="Login as admin"> Login as user </label>
+                        </div>
+                    </form>
+
+                    <button type="submit" onClick={handleLoginSubmit}>Login</button>
+
+                < /div>
+            </div>
+        );
+    } else if (direction === "admin") {
+        return <Redirect to="/admin"/>
+    }
 }
 export default LoginPage;
